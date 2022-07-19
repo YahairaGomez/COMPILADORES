5f5e0e8a4cebbb062c04a0721ba25946da6a9a5d
YahairaGomez <42581200+YahairaGomez@users.noreply.github.com>
2022-07-19 11:02:49 -0500

    Add files via upload

---
+++ P_FINAL_COMPILADORES/src/AnalizadorLexico.py
@@ -0,0 +1,136 @@
+from src.token import Class, Token
+
+
+class Lexer:
+    def __init__(self, text):
+        self.text = text
+        self.len = len(text)
+        self.pos = -1
+
+    def read_space(self):
+        while self.pos + 1 < self.len and self.text[self.pos + 1].isspace():
+            self.next_char()
+
+    def read_int(self):
+        lexeme = self.text[self.pos]
+        while self.pos + 1 < self.len and self.text[self.pos + 1].isdigit():
+            lexeme += self.next_char()
+        return int(lexeme)
+
+    def read_char(self):
+        self.pos += 1
+        lexeme = self.text[self.pos]
+        self.pos += 1
+        return lexeme
+
+    def read_string(self):
+        lexeme = ''
+        while self.pos + 1 < self.len and self.text[self.pos + 1] != '"':
+            lexeme += self.next_char()
+        self.pos += 1
+        return lexeme
+
+    def read_keyword(self):
+        lexeme = self.text[self.pos]
+        #Para ver si son alfanumericos, leo toda la linea
+        while self.pos + 1 < self.len and self.text[self.pos + 1].isalnum():
+            lexeme += self.next_char()
+        
+        if lexeme == 'if':
+            return Token(Class.IF, lexeme)
+        elif lexeme == 'else':
+            return Token(Class.ELSE, lexeme)
+        elif lexeme == 'while':
+            return Token(Class.WHILE, lexeme)
+        elif lexeme == 'int' or lexeme == 'char' or lexeme:
+            return Token(Class.TYPE, lexeme)
+        else:
+            print("Error de tipo de dato")
+        
+        return Token(Class.ID, lexeme)
+
+    def next_char(self):
+        self.pos += 1
+        if self.pos >= self.len:
+            return None
+        return self.text[self.pos]
+
+    def next_token(self):
+        self.read_space()
+        curr = self.next_char()
+        if curr is None:
+            return Token(Class.EOF, curr)
+        token = None
+        if curr.isalpha():
+            token = self.read_keyword()
+        elif curr.isdigit():
+            token = Token(Class.INT, self.read_int())
+        elif curr == '\'':
+            token = Token(Class.CHAR, self.read_char())
+        elif curr == '"':
+            token = Token(Class.STRING, self.read_string())
+        elif curr == '+':
+            token = Token(Class.PLUS, curr)
+        elif curr == '-':
+            token = Token(Class.MINUS, curr)
+        elif curr == '*':
+            token = Token(Class.STAR, curr)
+        elif curr == '/':
+            token = Token(Class.FWDSLASH, curr)
+        
+        elif curr == '!':
+            curr = self.next_char()
+            token = Token(Class.NOT, '!')
+            self.pos -= 1
+        elif curr == '=':
+            curr = self.next_char()
+            if curr == '=':
+                token = Token(Class.EQ, '==')
+            else:
+                token = Token(Class.ASSIGN, '=')
+                self.pos -= 1
+        elif curr == '<':
+            curr = self.next_char()
+            if curr == '=':
+                token = Token(Class.LTE, '<=')
+            else:
+                token = Token(Class.LT, '<')
+                self.pos -= 1
+        elif curr == '>':
+            curr = self.next_char()
+            if curr == '=':
+                token = Token(Class.GTE, '>=')
+            else:
+                token = Token(Class.GT, '>')
+                self.pos -= 1
+        elif curr == '(':
+            token = Token(Class.LPAREN, curr)
+        elif curr == ')':
+            token = Token(Class.RPAREN, curr)
+        elif curr == '[':
+            token = Token(Class.LBRACKET, curr)
+        elif curr == ']':
+            token = Token(Class.RBRACKET, curr)
+        elif curr == '{':
+            token = Token(Class.LBRACE, curr)
+        elif curr == '}':
+            token = Token(Class.RBRACE, curr)
+        elif curr == ';':
+            token = Token(Class.SEMICOLON, curr)
+        elif curr == ',':
+            token = Token(Class.COMMA, curr)
+        else:
+            self.die(curr)
+        return token
+
+    def lex(self):
+        tokens = []
+        while True:
+            curr = self.next_token()
+            tokens.append(curr)
+            if curr.class_ == Class.EOF:
+                break
+        return tokens
+
+    def die(self, char):
+        raise SystemExit("Unexpected character: {}".format(char))
